0247ee844b8008584f4a1d4cf02c4f06b46033e0
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2024-06-06 21:25:05 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Rudi portfolio made using Next.js
+            Made using Next.js
           </p>
 
           {/**
@@ -52,11 +52,11 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Developing projects"
+            words="👋 Hello!"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Rudish, a JavaScript Developer working on projects.
+            Hi! I&apos;m Rishan, a JavaScript Developer working on projects.
           </p>
        
            <a href="#projects"> 
